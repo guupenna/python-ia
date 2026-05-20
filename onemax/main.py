4f6e6d1a3a927d86eb155f50aa848afa86d0223f
Gustavo <gupenna1@gmail.com>
2026-05-20 16:31:07 -0300

    selection function without vetorization done

--- onemax/main.py
+++ onemax/main.py
@@ -21,8 +21,27 @@ def selection(population, scores):
 
     return population[selected]
 
+def crossover(parents):
+    half = len(parents) // 2
+    parents_a = parents[:half]
+    parents_b = parents[half:]
+
+    cross = np.random.randint(low=1, high=4)
+
+    childs_1 = []
+    for i in range(0, half):
+        childs_1.append(np.concatenate((parents_a[i][:cross], parents_b[i][cross:])))
+    
+    childs_2 = []
+    for i in range(0, half):
+        childs_2.append(np.concatenate((parents_b[i][:cross], parents_a[i][cross:])))
+
+    return childs_1 + childs_2
+
 population = np.random.randint(low=0, high=2, size=(10, 5))
 
 scores = fitness(population)
 
-print(selection(population, scores))
+selected = selection(population, scores)
+
+print(crossover(selected))
