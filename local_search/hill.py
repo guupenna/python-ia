2989278a57e90fe343d8bc7b4890e788973724ef
Gustavo <gupenna1@gmail.com>
2026-05-24 23:27:28 -0300

    random restart hill climbing implemented

--- local_search/hill.py
+++ local_search/hill.py
@@ -31,30 +31,74 @@ def gera_vizinhos(sol):
     return vizinhos
 
 
-def hill_climbing():
-    sol_atual = (0, 0, 0)
+def vanilla_hill_climbing():
+        sol_atual = (0, 0, 0)
 
-    while(True):
-        vizinhos = gera_vizinhos(sol_atual)
+        while(True):
+            vizinhos = gera_vizinhos(sol_atual)
 
-        if not vizinhos:
-            break
+            if not vizinhos:
+                break
 
-        melhor_vizinho = max(vizinhos, key=calcula_lucro)
+            melhor_vizinho = max(vizinhos, key=calcula_lucro)
 
-        if calcula_lucro(melhor_vizinho) <= calcula_lucro(sol_atual):
-            break
-        
-        sol_atual = melhor_vizinho
+            if calcula_lucro(melhor_vizinho) <= calcula_lucro(sol_atual):
+                break
+            
+            sol_atual = melhor_vizinho
 
-    return sol_atual
+        return sol_atual
 
+
+def random_restart_hill_climbing():
+    melhor_solucao_global = None
+    melhor_lucro_global = -1
+
+    for i in range(100):
+        # Procura solução inicial aleatória válida
+        while(True):
+            array = np.random.randint(0, 50, size=3)
+            sol_atual = (array[0], array[1], array[2])
+            if verifica_restricoes(sol_atual):
+                break
+
+        # Executa hill climbin com a solução inicial normalmente
+        while(True):
+            vizinhos = gera_vizinhos(sol_atual)
+
+            if not vizinhos:
+                break
+
+            melhor_vizinho = max(vizinhos, key=calcula_lucro)
+
+            if calcula_lucro(melhor_vizinho) <= calcula_lucro(sol_atual):
+                break
+
+            sol_atual = melhor_vizinho
+            
+        # Após uma "escalada", ele verifica se é a melhor solução já encontrada
+        lucro_atual = calcula_lucro(sol_atual)
+        if lucro_atual > melhor_lucro_global:
+            melhor_lucro_global = lucro_atual
+            melhor_solucao_global = sol_atual
+
+    return melhor_solucao_global
 
 if __name__ == "__main__":
-    solucao = hill_climbing()
+    # solucao = vanilla_hill_climbing()
 
+    # print("====== VANILLA HILL CLIMBING ======\n")
+    # print(f"Melhor plano de produção encontrado:")
+    # print(f"Unidades do produto A: {solucao[0]}")
+    # print(f"Unidades do produto B: {solucao[1]}")
+    # print(f"Unidades do produto C: {solucao[2]}")
+    # print(f"Lucro calculado: {calcula_lucro(solucao)}")
+
+    solucao_rr = random_restart_hill_climbing()
+
+    print("\n====== RANDOM RESTART HILL CLIMBING ======\n")
     print(f"Melhor plano de produção encontrado:")
-    print(f"Unidades do produto A: {solucao[0]}")
-    print(f"Unidades do produto B: {solucao[1]}")
-    print(f"Unidades do produto C: {solucao[2]}")
-    print(f"Lucro calculado: {calcula_lucro(solucao)}")
+    print(f"Unidades do produto A: {solucao_rr[0]}")
+    print(f"Unidades do produto B: {solucao_rr[1]}")
+    print(f"Unidades do produto C: {solucao_rr[2]}")
+    print(f"Lucro calculado: {calcula_lucro(solucao_rr)}")
